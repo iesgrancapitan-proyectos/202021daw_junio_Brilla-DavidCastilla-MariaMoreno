e07a1837046a8ea0599a327ac30331e94dda4bdb
mariamm99 <mariamm9961@gmail.com>
2021-03-29 10:01:30 +0200

    add create database

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"context"
+
 	"github.com/arangodb/go-driver"
 	"github.com/arangodb/go-driver/http"
 
@@ -21,4 +23,40 @@ func ConnectToDB() driver.Client {
 		panic(err)
 	}
 	return client
+}
+
+func CreateBD(client driver.Client) {
+	bd, err := client.CreateDatabase(context.Background(), "bd_brilla", nil)
+
+	if err != nil {
+		bd, err = client.Database(context.Background(), "bd_brilla")
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	bd.CreateCollection(context.Background(), "User", nil)
+
+	bd.CreateCollection(context.Background(), "Brillo", nil)
+
+	bd.CreateCollection(context.Background(), "Follows", &driver.CreateCollectionOptions{
+		Type: driver.CollectionTypeEdge,
+	})
+
+	bd.CreateCollection(context.Background(), "Author", &driver.CreateCollectionOptions{
+		Type: driver.CollectionTypeEdge,
+	})
+
+	bd.CreateCollection(context.Background(), "Comments", &driver.CreateCollectionOptions{
+		Type: driver.CollectionTypeEdge,
+	})
+
+	bd.CreateCollection(context.Background(), "Interactions", &driver.CreateCollectionOptions{
+		Type: driver.CollectionTypeEdge,
+	})
+
+	bd.CreateCollection(context.Background(), "Rebrilla", &driver.CreateCollectionOptions{
+		Type: driver.CollectionTypeEdge,
+	})
 }
