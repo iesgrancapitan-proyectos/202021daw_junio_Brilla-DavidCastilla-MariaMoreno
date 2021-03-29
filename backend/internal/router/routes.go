4f15d9683a1fc4971de8523194d250d0352007f0
David Castilla Ortiz <im@dcxo.dev>
2021-03-29 11:16:08 +0200

    Added functions to handle routes

--- backend/internal/router/routes.go
+++ backend/internal/router/routes.go
@@ -5,3 +5,15 @@ import "net/http"
 func getUser(rw http.ResponseWriter, r *http.Request) {
 	// TODO: Return info of user in JSON
 }
+
+func getUserBrights(rw http.ResponseWriter, r *http.Request) {
+	// TODO: Return brights of user in JSON
+}
+
+func postUser(rw http.ResponseWriter, r *http.Request) {
+	// TODO: Upload a new user
+}
+
+func postUserFollow(rw http.ResponseWriter, r *http.Request) {
+	// TODO: Follows a user
+}
