d5cb8e730754082971797fccf9a2512b7e8e1ff4
mariamm99 <mariamm9961@gmail.com>
2021-03-29 11:18:23 +0200

    add more routes

--- backend/internal/router/routes.go
+++ backend/internal/router/routes.go
@@ -17,3 +17,18 @@ func postUser(rw http.ResponseWriter, r *http.Request) {
 func postUserFollow(rw http.ResponseWriter, r *http.Request) {
 	// TODO: Follows a user
 }
+func postRebrilla(rw http.ResponseWriter, r *http.Request) {
+	//TODO: Crea un brillo pasado por JSON
+}
+
+func postInteraction(rw http.ResponseWriter, r *http.Request) {
+	//TODO : Interactua con un brillo y el tipo de intereción por el json mandado
+}
+
+func postComment(rw http.ResponseWriter, r *http.Request) {
+	//TODO: Crear un brillo respuesta a otro Brillo
+}
+
+func postBright(rw http.ResponseWriter, r *http.Request) {
+	// TODO: Crea un brillo
+}
