ebe803768e26f4dfd05800e989641b0312b9383d
mariamm99 <mariamm9961@gmail.com>
2021-03-29 10:46:23 +0200

    created type User

---
+++ backend/internal/models/User.go
@@ -0,0 +1,13 @@
+package models
+
+type User struct {
+	User       string `json:"_key,omitempty"`
+	Email      string `json:"email,omitempty"`
+	Password   string `json:"password,omitempty"`
+	Name       string `json:"name,omitempty"`
+	Bio        string `json:"bio,omitempty"`
+	Birthday   int64  `json:"birthday,omitempty"`
+	ProfileImg string `json:"profile_img,omitempty"`
+	CreatedAt  int64  `json:"created_at,omitempty"`
+	UpdateAt   int64  `json:"update_at,omitempty"`
+}
