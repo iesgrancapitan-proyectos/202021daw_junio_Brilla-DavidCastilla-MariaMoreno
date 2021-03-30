0e3c7b621a39dd12b239b059960cc5cec191ae09
mariamm99 <mariamm9961@gmail.com>
2021-03-30 11:21:15 +0200

    add comments for querys

--- backend/internal/database/queries/queries.go
+++ backend/internal/database/queries/queries.go
@@ -19,28 +19,52 @@ var InsertUserQuery string
 var GetUserQuery string
 
 //go:embed deactivateUser.aql
+// DesactivateUserQuery need one variable
+// - @username
 var DeactivateUserQuery string
 
 //go:embed deleteBrillo.aql
+//DeleteBrilloQuery need one variable
+// -@brillo_key
 var DeleteBrilloQuery string
 
 //go:embed getBrilloById.aql
+// GetBrilloByIdQuery need one variable
+// -@id
 var GetBrilloByIdQuery string
 
 //go:embed getComments.aql
+// GetCommentsQuery need one variable
+// -@brillo
 var GetCommentsQuery string
 
 //go:embed interaction.aql
+// InteractionQuery need 3 variables
+// -@brilloid
+// -@type
+// -@userid
 var InteractionQuery string
 
 //go:embed newFollow.aql
+// NewFollowQuery need 2 variables
+// -@follower
+// -@followed
 var NewFollowQuery string
 
 //go:embed rebrillo.aql
+// RebrilloQuery need 2 variables
+// -@brilloId
+// -@userId
 var RebrilloQuery string
 
 //go:embed recomendationUser.aql
+// RecomendationUserQuery need 1 variables
+// -@username
 var RecomendationUserQuery string
 
 //go:embed commentBrillo.aql
+// CommentQuery need 2 variables
+// -@username
+// -@content
+// -@media
 var CommentBrilloQuery string
