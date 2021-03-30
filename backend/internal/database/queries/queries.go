d15e7d59d7f9792834fca8c23cec8aa3df14ea78
David Castilla Ortiz <im@dcxo.dev>
2021-03-30 10:44:09 +0200

    Added queries to queries file

--- backend/internal/database/queries/queries.go
+++ backend/internal/database/queries/queries.go
@@ -17,3 +17,30 @@ var InsertUserQuery string
 // GetUserQuery needs one variable
 // - @username: Username
 var GetUserQuery string
+
+//go:embed deactivateUser.aql
+var DeactivateUserQuery string
+
+//go:embed deleteBrillo.aql
+var DeleteBrilloQuery string
+
+//go:embed getBrilloById.aql
+var GetBrilloByIdQuery string
+
+//go:embed getComments.aql
+var GetCommentsQuery string
+
+//go:embed interaction.aql
+var InteractionQuery string
+
+//go:embed newFollow.aql
+var NewFollowQuery string
+
+//go:embed rebrillo.aql
+var RebrilloQuery string
+
+//go:embed recomendationUser.aql
+var RecomendationUserQuery string
+
+//go:embed commentBrillo.aql
+var CommentBrilloQuery string
