eb70b5c9aee159db29bf8b03524490393518283d
mariamm99 <mariamm9961@gmail.com>
2021-04-06 19:23:10 +0200

    creando index

--- frontend/src/pages/Index.svelte
+++ frontend/src/pages/Index.svelte
@@ -3,37 +3,33 @@
 </script>
 
 <section>
-
     <form>
         <div>
             <input type="text" name="user" />
             <label for="user">User</label>
         </div>
-    
+
         <div>
             <input type="password" name="password" id="password" />
             <label for="password">Password</label>
         </div>
-    
+
         <div class="buttons">
             <Link>Sign up</Link>
-            <input type="submit" value="Log in">
+            <input type="submit" value="Log in" />
         </div>
-        
     </form>
-    
-    <hr>
-    
+
+    <hr />
+
     <div class="google_facebook">
         <Link class="google">Log in with Google</Link>
         <Link class="facebook">Log in with Facebook</Link>
     </div>
- 
-    
 </section>
 
 <style lang="scss">
-    section{
+    section {
         padding: 16px;
     }
     form > div {
@@ -58,30 +54,45 @@
                 border-radius: 8px;
                 + label {
                     top: 0%;
-            
                 }
             }
         }
     }
 
-    .buttons{
+    .buttons {
         display: flex;
         justify-content: space-between;
-        
-        :global(a), input{
+
+        :global(a),
+        input {
             padding: 8px;
             flex-basis: 30%;
-            
         }
     }
 
-    .google_facebook{
+    .google_facebook {
         display: flex;
         flex-direction: column;
         text-align: center;
-        :global(a){
+
+        :global(a) {
+            color: white;
+          margin: 12px;
+        }
+
+        :global(.google) {
+            background: linear-gradient(91.44deg, #db4437 0%, #dc5429 100%);
+            box-shadow: 0px 4px 8px rgba(219, 68, 55, 0.3);
+            border-radius: 12px;
             padding: 8px;
-                        
+        }
+
+        :global(.facebook) {
+            background: linear-gradient(91.44deg, #4267b2 0%, #4254b2 100%);
+            box-shadow: 0px 4px 8px rgba(66, 103, 178, 0.3);
+            border-radius: 12px;
+            padding: 8px;
+
         }
     }
 </style>
