2c2ac38104ab6512ef23bef273b430ec876db6a3
mariamm99 <mariamm9961@gmail.com>
2021-04-06 18:06:16 +0200

    variables globales de css

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -14,6 +14,15 @@
 </Router>
 
 <style global>
+    :root{
+        --primary-color: #F9C55F ;
+        --secondary-color: #FB5666;
+        --dark-primary-color: #F7B024;
+        --light-primary-color: #FBDA9A;
+        --dark-seconday-color: #FA4553;
+        --light-seconday-color: #FC8994;
+    }
+
     * {
         margin: 0;
         padding: 0;
---
+++ frontend/src/pages/Index.svelte
@@ -0,0 +1,14 @@
+<style>
+    input{
+        border-color: var(--primary-color);
+    }
+</style>
+<form>
+    <label for="user">User</label>
+    <input type="text" name="user" placeholder="user/email">
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password" placeholder="password">
+
+    <input type="button" value="Sign Up">
+    <input type="submit" value="Sign Up">
+</form>
