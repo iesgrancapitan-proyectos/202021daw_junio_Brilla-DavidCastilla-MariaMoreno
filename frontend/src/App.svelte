580c540a1e28259d507fa93aae115a33ff9811a4
mariamm99 <mariamm9961@gmail.com>
2021-04-06 19:07:08 +0200

    changed css global de css

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -26,5 +26,13 @@
     * {
         margin: 0;
         padding: 0;
+        box-sizing: border-box;
     }
+
+    a{
+        text-decoration: none;
+        color:  black;
+    }
+
+
 </style>
--- frontend/src/pages/Index.svelte
+++ frontend/src/pages/Index.svelte
@@ -1,14 +1,87 @@
-<style>
-    input{
-        border-color: var(--primary-color);
+<script>
+    import { Link } from "svelte-routing";
+</script>
+
+<section>
+
+    <form>
+        <div>
+            <input type="text" name="user" />
+            <label for="user">User</label>
+        </div>
+    
+        <div>
+            <input type="password" name="password" id="password" />
+            <label for="password">Password</label>
+        </div>
+    
+        <div class="buttons">
+            <Link>Sign up</Link>
+            <input type="submit" value="Log in">
+        </div>
+        
+    </form>
+    
+    <hr>
+    
+    <div class="google_facebook">
+        <Link class="google">Log in with Google</Link>
+        <Link class="facebook">Log in with Facebook</Link>
+    </div>
+ 
+    
+</section>
+
+<style lang="scss">
+    section{
+        padding: 16px;
+    }
+    form > div {
+        position: relative;
+        margin: 16px 0;
+        label {
+            background-color: white;
+            margin: 0 8px;
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            left: 16px;
+        }
+
+        input {
+            padding: 16px;
+            display: block;
+            width: 100%;
+            &:focus {
+                outline: none;
+                border-color: var(--primary-color);
+                border-radius: 8px;
+                + label {
+                    top: 0%;
+            
+                }
+            }
+        }
+    }
+
+    .buttons{
+        display: flex;
+        justify-content: space-between;
+        
+        :global(a), input{
+            padding: 8px;
+            flex-basis: 30%;
+            
+        }
+    }
+
+    .google_facebook{
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        :global(a){
+            padding: 8px;
+                        
+        }
     }
 </style>
-<form>
-    <label for="user">User</label>
-    <input type="text" name="user" placeholder="user/email">
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password" placeholder="password">
-
-    <input type="button" value="Sign Up">
-    <input type="submit" value="Sign Up">
-</form>
