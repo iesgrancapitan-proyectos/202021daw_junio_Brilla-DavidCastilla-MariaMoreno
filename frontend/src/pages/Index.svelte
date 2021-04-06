27e25a84e752374714902ad0f8fb02de0fdf41e7
mariamm99 <mariamm9961@gmail.com>
2021-04-06 19:39:49 +0200

    add index

--- frontend/src/pages/Index.svelte
+++ frontend/src/pages/Index.svelte
@@ -5,7 +5,7 @@
 <section>
     <form>
         <div>
-            <input type="text" name="user" />
+            <input type="text" name="user" id="user" />
             <label for="user">User</label>
         </div>
 
@@ -42,18 +42,23 @@
             top: 50%;
             transform: translateY(-50%);
             left: 16px;
+            font-weight: bold;
+            padding: 0px 2px;
         }
 
         input {
             padding: 16px;
             display: block;
             width: 100%;
+            border: 2px solid black;
+            border-radius: 8px;
+
             &:focus {
                 outline: none;
-                border-color: var(--primary-color);
-                border-radius: 8px;
+                border: 2px solid var(--primary-color);
                 + label {
                     top: 0%;
+                    color: var(--dark-primary-color);
                 }
             }
         }
@@ -67,6 +72,23 @@
         input {
             padding: 8px;
             flex-basis: 30%;
+            text-align: center;
+            border-radius: 12px;
+            font-weight: bold;
+
+        }
+
+        :global(a) {
+            color: var(--secondary-color);
+            border: 2px solid var(--secondary-color);
+            filter: drop-shadow(0px 4px 8px rgba(249, 197, 95, 0.3));
+        }
+
+        input{
+            background-color: var(--primary-color);
+            border: 2px solid var(--primary-color);
+
+
         }
     }
 
@@ -77,22 +99,20 @@
 
         :global(a) {
             color: white;
-          margin: 12px;
+            margin: 12px;
+            padding: 16px;
         }
 
         :global(.google) {
             background: linear-gradient(91.44deg, #db4437 0%, #dc5429 100%);
             box-shadow: 0px 4px 8px rgba(219, 68, 55, 0.3);
             border-radius: 12px;
-            padding: 8px;
         }
 
         :global(.facebook) {
             background: linear-gradient(91.44deg, #4267b2 0%, #4254b2 100%);
             box-shadow: 0px 4px 8px rgba(66, 103, 178, 0.3);
             border-radius: 12px;
-            padding: 8px;
-
         }
     }
 </style>
