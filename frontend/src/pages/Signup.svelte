a40d4e110219403248ab74a1c65fa7ea168fb733
mariamm99 <mariamm9961@gmail.com>
2021-04-08 18:08:18 +0200

    more change in signup

--- frontend/src/pages/Signup.svelte
+++ frontend/src/pages/Signup.svelte
@@ -7,38 +7,85 @@
         passwordError = "";
     let name = "",
         nameError = "";
+    let birthday = "",
+        birthdayError = "";
+    let email = "",
+        emailError = "";
+
+        function signup() {
+            
+        }
 </script>
 
-<form>
-    <Input
-        type="text"
-        label="Name"
-        id="name"
-        bind:value={name}
-        errorMessage={nameError}
-    />
+<section>
+    <h2>Create Account</h2>
+    <form on:submit|preventDefault={signup} >
+        <Input
+            type="text"
+            label="Name"
+            id="name"
+            bind:value={name}
+            errorMessage={nameError}
+        />
 
-    <Input
-        type="text"
-        label="User"
-        id="username"
-        bind:value={username}
-        errorMessage={usernameError}
-    />
+        <Input
+            type="email"
+            label="Email"
+            id="email"
+            bind:value={email}
+            errorMessage={emailError}
+        />
 
-    <Input
-        type="password"
-        label="Password"
-        id="password"
-        bind:value={password}
-        errorMessage={passwordError}
-    />
+        <Input
+            type="text"
+            label="User"
+            id="username"
+            bind:value={username}
+            errorMessage={usernameError}
+        />
 
-    <div>
-        <input type="submit" value="Singup" />
-    </div>
-</form>
+        <Input
+            type="password"
+            label="Password"
+            id="password"
+            bind:value={password}
+            errorMessage={passwordError}
+        />
+
+        <Input
+            type="text"
+            label="Birthday"
+            id="birthday"
+            bind:value={birthday}
+            errorMessage={birthdayError}
+        />
+
+        <div>
+            <input type="submit" value="Sign up" />
+        </div>
+    </form>
+</section>
 
 <style lang="scss">
-</style>
+    section {
+        padding: 32px;
+    }
 
+    form {
+        display: grid;
+        grid-gap: 16px;
+        input {
+            padding: 8px 16px;
+            text-align: center;
+            border-radius: 12px;
+            font-weight: bold;
+            background-color: var(--primary-color);
+            border: 2px solid var(--primary-color);
+        }
+    }
+
+    h2 {
+        text-align: center;
+        margin-bottom: 32px;
+    }
+</style>
