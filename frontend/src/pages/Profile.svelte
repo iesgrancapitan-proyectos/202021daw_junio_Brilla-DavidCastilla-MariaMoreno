a490d18e532314f1fa1a760bc0b95b81be778f50
mariamm99 <mariamm9961@gmail.com>
2021-04-11 12:04:56 +0200

    started with profile

---
+++ frontend/src/pages/Profile.svelte
@@ -0,0 +1,43 @@
+<script>
+let user="";
+let name="";
+let username="", bio="", followed="", followers="", nbrillos="";
+</script>
+
+
+
+<section >
+    <div>
+        <svg height="100" width="100">
+            <circle
+                cx="200"
+                cy="200"
+                r="200"
+                stroke="#f9c55f"
+                stroke-width="3"
+                fill="#f9c55f"
+            />
+        </svg>
+    </div>
+    <p>{username}</p>
+
+    <img src="/img/{user}" alt="img perfil">
+
+    <p>{name}</p>
+
+    <div>
+        <p>{bio}</p>
+
+        <div>
+           <p>{followed} Followed</p>
+           <p>{followers} followers</p>
+           <p>{nbrillos} Brillos</p>
+
+        </div>
+    </div>
+    <button>Follow/Following</button>
+    <hr>
+</section>
+
+<style lang="scss">
+</style>
