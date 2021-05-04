111be356d6c4962c27f94d56dab74c9db38e15cc
David Castilla Ortiz <im@dcxo.dev>
2021-05-04 17:11:30 +0200

    Google oauth

--- backend/internal/router/oauthRoutes.go
+++ backend/internal/router/oauthRoutes.go
@@ -1,9 +1,19 @@
 package router
 
 import (
+	"brilla/internal/database/queries"
+	"brilla/internal/middleware"
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
+	arango "github.com/arangodb/go-driver"
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
@@ -13,24 +23,101 @@ var googleID = os.Getenv("ID_OAUTH")
 
 var apiURL = os.Getenv("API_URL")
 
+var googleOauthConfig = &oauth2.Config{
+	ClientID:     googleID,
+	ClientSecret: googleSecret,
+	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
+	RedirectURL:  apiURL + "/auth/google/confirm",
+	Endpoint:     google.Endpoint,
+}
+
+type googleResponse struct {
+	Email          string `json:"email"`
+	Verified_email bool   `json:"verified_email"`
+	Name           string `json:"name"`
+	Picture        string `json:"picture"`
+}
+
 // googleAuth: /auth/google
 func (server *Server) googleAuth(wr http.ResponseWriter, r *http.Request) {
 
-	conf := &oauth2.Config{
-		ClientID:     googleID,
-		ClientSecret: googleSecret,
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"}, //, "https://www.googleapis.com/auth/userinfo.profile"},
-		RedirectURL:  apiURL + "/auth/google/confirm",
-		Endpoint:     google.Endpoint,
-	}
-
-	url := conf.AuthCodeURL("state")
+	url := googleOauthConfig.AuthCodeURL("state")
 	http.Redirect(wr, r, url, http.StatusTemporaryRedirect)
 
 }
 
-func (server *Server) googleAuthConfirm(wr http.ResponseWriter, r *http.Request) {
+func randomString() string {
+	b := make([]byte, 10)
+	rand.Read(b)
+	var sb strings.Builder
+	base64.NewEncoder(base64.RawURLEncoding, &sb).Write(b)
+	return sb.String()
+}
 
-	println("asdf")
+func (server *Server) googleAuthConfirm(rw http.ResponseWriter, r *http.Request) {
+
+	// FIX: Handle errors
+
+	code := r.FormValue("code")
+	tkn, _ := googleOauthConfig.Exchange(context.Background(), code)
+
+	resRaw, _ := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + tkn.AccessToken)
+	var res googleResponse
+	json.NewDecoder(resRaw.Body).Decode(&res)
+
+	username := "estrella-" + randomString()
+	// validación email
+	email := res.Email
+	bio := ""
+	name := res.Name
+
+	profileImg := res.Picture
+
+	_, err := server.database.Query(context.Background(), queries.InsertUserQuery, map[string]interface{}{
+		"username":    username,
+		"email":       email,
+		"bio":         bio,
+		"password":    nil,
+		"name":        name,
+		"birthday":    nil,
+		"profile_img": profileImg,
+		"is_active":   true,
+	})
+	if arango.IsConflict(err) {
+		writeError(rw, "Error. Conflict user. "+err.Error(), http.StatusConflict)
+		return
+	} else if err != nil {
+		writeError(rw, "Error. Creating user. "+err.Error(), http.StatusConflict)
+		return
+	}
+
+	expirationTime := time.Now().AddDate(0, 0, 3)
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    username,
+		ExpiresAt: expirationTime.Unix(),
+	})
+
+	signed, err := token.SignedString(middleware.JwtKey)
+	if err != nil {
+		writeError(rw, "Error signing token. "+err.Error(), http.StatusInternalServerError)
+	}
+
+	http.SetCookie(rw, &http.Cookie{
+		Name:     "token",
+		Value:    signed,
+		Secure:   false,
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode,
+		Expires:  expirationTime,
+		Path:     "/",
+	})
+
+	rw.Header().Add("X-Token", signed)
+
+	http.Redirect(rw, r, "/", http.StatusFound)
+	json.NewEncoder(rw).Encode(map[string]string{
+		"username": username,
+	})
 
 }
